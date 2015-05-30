76763eeb5454fe60bf97210664e2372703ccd97c
ColsaX <serediuc.mircea@gmail.com>
2015-05-30 20:28:56 +0300

    Test endava

---
+++ WebstormProjects/testEndava/js/testEnadava.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+
+    var url= "https://www.dropbox.com/home?preview=form-validation.json";
+    var sn=$('#sn').val();
+    var icons = {
+        header: "ui-icon-circle-arrow-e",
+        activeHeader: "ui-icon-circle-arrow-s"
+    };
+    $( ".accordion" ).accordion({
+        icons: icons
+    });
+
+
+
+    $('.submit').click(function () {
+
+        if(sn==""){
+            $('.required').append(' (Please supply)')
+            $('.sn').addClass('has-error');
+        }else{
+            $.ajax({
+                dataType: "json",
+                url: url,
+                success: success
+            });
+
+        }
+
+
+    });
+
+
+
+
+
+});
